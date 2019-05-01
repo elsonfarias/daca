3b9c8d193b189939ca007e12f88b471e6e7f5ac6
elsonfarias <elsonfar@gmail.com>
2019-05-01 10:10:44 -0300

    Mudanças no IsbnValidador

--- src/main/java/validator/IsbnValidador.java
+++ src/main/java/validator/IsbnValidador.java
@@ -1,32 +1,32 @@
 package validator;
 
-import java.io.Serializable;
-
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
 
 import org.apache.commons.validator.routines.ISBNValidator;
 
-
-@FacesValidator("isbnValidator")
-public class IsbnValidador extends ISBNValidator implements Serializable{
+@FacesValidator("isbnValidador")
+public class IsbnValidador implements Validator<String> {
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+	ISBNValidator validador = new ISBNValidator();
 	
-	public IsbnValidador() {
-		// TODO Auto-generated constructor stub
-	}
-
+	
 	@Override
-	public String validate(String code) {
-		// TODO Auto-generated method stub
-		return super.validate(code);
+	public void validate(FacesContext context, UIComponent component, String value) throws ValidatorException {
+		String result = validador.validate(value);
+		
+		
+		if (result == null || result.trim().length() < 10) {
+			FacesMessage msg = new FacesMessage("Erro de validação do ISBN.");
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ValidatorException(msg);
+		}
+		
 	}
-	
+
+		
 }
-
-
-
-	
